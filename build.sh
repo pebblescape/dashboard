6f87663ca612707a8c362cba5381e9e456fc44ee
Kristjan Rang <mail@rang.ee>
2015-03-23 20:07:16 +0200

    Build into separate branch

--- build.sh
+++ build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-mkdir -p ./build
-ember build --environment="production" -o ./tmpbuild
-cp -r ./tmpbuild/* ./build/
-rm -r ./tmpbuild
-cp ./build/index.html ./build/`git rev-parse --short HEAD`.html
+ember build --environment="production" -o ./build
