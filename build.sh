9a5dc6c9c62759e35fdb400f51e10b658daea78b
Kristjan Rang <mail@rang.ee>
2015-03-23 22:13:05 +0200

    Deploy test

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ ssh-add deploy.pem
 export GIT_COMMITTER_EMAIL="travis@rang.ee"
 export GIT_COMMITTER_NAME="Travis CI"
 
-git fetch || exit
+git fetch origin build:origin/build || exit
 git checkout -t -b build origin/build || exit
 git merge "$TRAVIS_COMMIT" || exit
 git rebase master
