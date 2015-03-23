cdf5d7c4adc584b3f37c8827fb8c9328f2e4e0d3
Kristjan Rang <mail@rang.ee>
2015-03-23 21:47:09 +0200

    Deploy test

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+eval `ssh-agent -s`
 chmod 600 deploy.pem
 ssh-add deploy.pem
 
 export GIT_COMMITTER_EMAIL="travis@rang.ee"
 export GIT_COMMITTER_NAME="Travis CI"
 
-git checkout build || exit
+git checkout origin/build || exit
 git merge "$TRAVIS_COMMIT" || exit
 git rebase master
 
@@ -14,4 +15,4 @@ ember build --environment="production" -o ./build
 
 git add --all ./build
 git commit -m "Build $TRAVIS_COMMIT"
-git push
+git push origin build
