9f9f622921f5ed1aeb8b392483a25806824fd135
Kristjan Rang <mail@rang.ee>
2015-03-23 21:43:12 +0200

    Trying deploy fix

--- build.sh
+++ build.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+chmod 600 deploy.pem
+ssh-add deploy.pem
+
+export GIT_COMMITTER_EMAIL="travis@rang.ee"
+export GIT_COMMITTER_NAME="Travis CI"
+
+git checkout build || exit
+git merge "$TRAVIS_COMMIT" || exit
+git rebase master
+
 ember build --environment="production" -o ./build
+
+git add --all ./build
+git commit -m "Build $TRAVIS_COMMIT"
+git push
