954bc712445874327d4c6971b1092cfb4a71dbf7
Kristjan Rang <mail@rang.ee>
2015-03-23 23:15:37 +0200

    Deploy test

--- build.sh
+++ build.sh
@@ -12,7 +12,6 @@ git config user.name "Travis CI"
 git remote add github git@github.com:pebblescape/dashboard.git || exit
 git fetch github || exit
 git checkout -t -b build github/build || exit
-git merge github/master --no-edit || exit
 
 ember build --environment="production" -o ./build
 
