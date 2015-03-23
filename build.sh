bf317be5bcdad6cb2f95ed0da75f8f6b4b08ebcd
Kristjan Rang <mail@rang.ee>
2015-03-23 22:48:33 +0200

    Deploy test

--- build.sh
+++ build.sh
@@ -7,13 +7,14 @@ ssh-add deploy.pem
 git config user.email "travis@rang.ee"
 git config user.name "Travis CI"
 
-git remote add github https://github.com/pebblescape/dashboard.git
-git fetch github build:github/build || exit
-git checkout -t -b build github/build || exit
+git remote rm origin || exit
+git remote add origin https://github.com/pebblescape/dashboard.git || exit
+git fetch origin build:origin/build || exit
+git checkout -t -b build origin/build || exit
 git rebase master || exit
 
 ember build --environment="production" -o ./build
 
 git add --all ./build
 git commit -m "Build $TRAVIS_COMMIT"
-git push -u github build
+git push -u origin build
