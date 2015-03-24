542aef5ae66052079fd8e3e2acb6225935d0d9ea
Kristjan Rang <mail@rang.ee>
2015-03-24 10:49:42 +0200

    Fix build

--- build.sh
+++ build.sh
@@ -12,11 +12,11 @@ git config user.name "Travis CI"
 
 git remote add github git@github.com:pebblescape/dashboard.git
 git fetch github
-git checkout -t -b build github/build
-git merge github/master --no-edit
+#git checkout -t -b build github/build
+git merge github/build --no-edit
 
 ember build --environment="production" -o ./build
 
 git add --all .
 git commit -m "Build $TRAVIS_COMMIT"
-git push github build
+git push github github/build
