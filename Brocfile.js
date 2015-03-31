691b0c9caaf091c5742347175be24ac8ef185bf4
Kristjan Rang <mail@rang.ee>
2015-03-31 20:10:33 +0300

    Set up S3 deploy

--- Brocfile.js
+++ Brocfile.js
@@ -2,7 +2,11 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+var app = new EmberApp({
+  fingerprint: {
+    prepend: '//d1uq7xqh8ryyi8.cloudfront.net/'
+  }
+});
 
 app.import('bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js');
 app.import('bower_components/bootbox/bootbox.js');
--- build.sh
+++ build.sh
@@ -3,20 +3,5 @@
 set -e
 set -x
 
-eval `ssh-agent -s`
-chmod 600 .travis/deploy.pem
-ssh-add .travis/deploy.pem
-
-git config user.email "travis@rang.ee"
-git config user.name "Travis CI"
-
-git remote add github git@github.com:pebblescape/dashboard.git
-git fetch github
-git checkout -t -b build github/build
-git merge master --no-edit
-
 ember build --environment="production" -o ./build
-
-git add --all ./build
-git commit -m "Build $TRAVIS_COMMIT"
-git push github build
+cp build/index.html build/index.$(git rev-parse --short HEAD).html
