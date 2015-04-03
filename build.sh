76233d4c5d4427af91cb793f6af66a861c03d53d
Kristjan Rang <mail@rang.ee>
2015-04-03 22:16:25 +0300

    Fix build script

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -x
 
 ref=$(git rev-parse --short HEAD)
 ember build --environment="production" -o ./build
-sed -i '' "s/refplaceholder/$ref/g" ./build/index.html
+perl -pi -e "s/refplaceholder/$ref/g" ./build/index.html
 cp build/index.html build/index.$ref.html
